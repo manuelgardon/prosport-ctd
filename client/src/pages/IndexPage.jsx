bff506feea1639c3f39f0ef0ff15305c28d23839
Aris Bello M <arisbllomartinez@gmail.com>
2023-10-23 21:13:12 -0500

    Corregido error visualizacionpagina registro

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -18,16 +18,16 @@ export default function IndexPage() {
             <ul className="grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 text-center">
                 {characters.length > 0 && characters.splice(0, 10).map(character => (
                     <li key={character.id} className="">
-                        <div className="flex rounded-t-xl h-50">
+                        <div className="flex rounded-t-xl">
 
                         <img src={character.image} alt={character.name} className="rounded-t border-collapse"/>
                         <img src="src/Icons/Like.png" alt="" className="w-5 h-5 my-2 relative right-7"/>
                         </div>
-                        <div className="">
+                       
 
                         <p className="bg-[#18181B] text-[#17B289] text-2xl">{character.name}</p>
                         <p className="border-2 border-[#FF9B27] text-[#FF9B27] rounded-b">{character.species}</p>
-                        </div>
+                        
                     </li>
                 ))}
             </ul>
