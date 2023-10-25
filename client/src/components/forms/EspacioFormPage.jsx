ddfba32dcc25d90354c0fcd57375fb940a9ccc2c
Aris Bello M <arisbllomartinez@gmail.com>
2023-10-25 17:21:35 -0500

    Estilos de los input check y acomodo render desktop

--- client/src/components/forms/EspacioFormPage.jsx
+++ client/src/components/forms/EspacioFormPage.jsx
@@ -34,7 +34,7 @@ function EspacioFormPage() {
     return (
         <section className='bg-gray-500 dark:bg-[#18181B] grow'>
 
-        <div className="flex flex-col items-center justify-center px-6 pt-[100px] md:h-screen lg:pt-0 mb-4">
+        <div className="flex flex-col items-center justify-center px-6 pt-[100px] md:h-screen lg:pt-20 mb-4">
             <Link to={'/'} className="flex gap-2 items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img className="w-9 h-9 object-contain" src={logo} alt="logo" />
                 Pro-Sport
@@ -48,7 +48,7 @@ function EspacioFormPage() {
                         <input type="text" placeholder="title, for example: My amazing apartment"
                             value={nombre}
                             onChange={e => setNombre(e.target.value)}
-                            className="w-full mb-2 border my-1 py-2 px-3 rounded-2xl"
+                            className="w-full mb-2 border my-1 py-2 px-3 rounded-2xl bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         />
                         <h2 className="block mb-2 text-2sm font-medium text-gray-900 dark:text-white">Deporte</h2>
                         <p>
@@ -59,7 +59,7 @@ function EspacioFormPage() {
                         <input type="text" placeholder="address"
                             value={deporte}
                             onChange={e => setDeporte(e.target.value)}
-                            className="w-full mb-2 border my-1 py-2 px-3 rounded-2xl"
+                            className="w-full mb-2 border my-1 py-2 px-3 rounded-2xl bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         />
                         {/*<h2 className="block mb-2 text-2sm font-medium text-gray-900 dark:text-white">Description</h2>
                             <p>
@@ -77,28 +77,28 @@ function EspacioFormPage() {
                                 <input type="text"
                                     value={diaSemana}
                                     onChange={e => setDiasemana(e.target.value)} placeholder="12"
-                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl' />
+                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl w-full bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                             </div>
                             <div>
                                 <h3 className="mt-2 -mb-1 block  text-2sm font-medium text-gray-900 dark:text-white">Cantidad de participantes</h3>
                                 <input type="number"
                                     value={cantidadDeParticipantes}
                                     onChange={e => setCantidadDeParticipantes(e.target.value)} placeholder="12"
-                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl' />
+                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl w-full bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                             </div>
                             <div>
                                 <h3 className="mt-2 -mb-1 block  text-2sm font-medium text-gray-900 dark:text-white">Hora inicio</h3>
                                 <input type="number"
                                     value={horaInicio}
                                     onChange={e => setHoraInicio(e.target.value)} placeholder="12"
-                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl' />
+                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl w-full bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                             </div>
                             <div>
                                 <h3 className="mt-2 -mb-1 block  text-2sm font-medium text-gray-900 dark:text-white">Hora fin</h3>
                                 <input type="number"
                                     value={horaFin}
                                     onChange={e => setHoraFin(e.target.value)} placeholder="12"
-                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl' />
+                                    className='mb-1 border my-1 py-1 px-2 rounded-2xl w-full bg-gray-50 border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                             </div>
                         </section>
                         <button className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-700 dark:hover:bg-green-500 dark:focus:ring-primary-800">Save</button>
