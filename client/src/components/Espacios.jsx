78350dc10254bf7652297844ca520c5367325bcd
Manuel Gardon <manugardon801@gmail.com>
2023-11-05 20:02:03 -0300

    Correccion puertos y propiedades de espacios | eliminacion paginado

--- client/src/components/Espacios.jsx
+++ client/src/components/Espacios.jsx
@@ -6,7 +6,7 @@ import Filters from "../components/Filters"
 export default function Espacios({ espacios, setEspacios, changeFilters }){
 
     useEffect(() => {
-        axios.get('http://localhost:8085/api/espacios').then((response) => {
+        axios.get('http://localhost:1234/api/espacios').then((response) => {
             setEspacios(response.data)
         })
     }, [setEspacios])
@@ -18,7 +18,7 @@ export default function Espacios({ espacios, setEspacios, changeFilters }){
         <main className="dark:bg-[#18181B] dark:text-white w-900 flex flex-col justify-center items-center px-10 py-[80px] lg:py-[145px] grow">
             <Filters onChange={changeFilters}/>
             <ul className="grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 text-center">
-                {espacios.length > 0 && espacios.slice(0, 10).map(espacio => (
+                {espacios.length > 0 && espacios.map(espacio => (
                     <li key={espacio._id}>
                         <div className="rounded-2xl mb-2 flex">
                             {espacio.fotos?.[0] && (
