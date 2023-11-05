4f6ccb0fff7fa25034da73b2582146882fbe2472
Aris Bello M <arisbellomartinez@gmail.com>
2023-11-05 13:24:53 -0500

    Doungrade o como se escriba

--- client/src/components/Espacios.jsx
+++ client/src/components/Espacios.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react"
 import axios from "axios"
 import Filters from "../components/Filters"
 
-
 export default function Espacios({ espacios, setEspacios, changeFilters }){
 
     useEffect(() => {
@@ -16,11 +15,7 @@ export default function Espacios({ espacios, setEspacios, changeFilters }){
     
     return ( 
     
-        <main className="dark:bg-[#18181B] dark:text-white w-900 flex flex-col justify-center items-center px-10 py-[80px] grow">
-            <div className="w-50 flex justify-center relative">
-                <input type="text" className="p-1 px-2 w-80 rounded-xl text-xl h-10 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar..." />
-                <button className="rounded-xl dark:bg-green-700 dark:hover:bg-green-500 dark:focus:ring-primary-800 h-10 absolute right-0 ">Buscar</button>
-            </div>
+        <main className="dark:bg-[#18181B] dark:text-white w-900 flex flex-col justify-center items-center px-10 py-[80px] lg:py-[145px] grow">
             <Filters onChange={changeFilters}/>
             <ul className="grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 text-center">
                 {espacios.length > 0 && espacios.map(espacio => (
