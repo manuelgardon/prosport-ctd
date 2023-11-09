515de4f284e4a0250b91d7e88ba7fe64a04183ce
Aris Bello M <arisbellomartinez@gmail.com>
2023-11-09 15:22:39 -0500

    Boton chueco

--- client/src/pages/EspaciosPage.jsx
+++ client/src/pages/EspaciosPage.jsx
@@ -106,7 +106,7 @@ export default function EspaciosPage() {
                             EDITAR
                             </Link>
                         </button>
-                        <button onClick={() => handleDelete(espacio._id)} className="grow-0 shrink text-[#00FF9D] bg-[#131818] border-[#00FF9D] border dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 ml-2">
+                        <button onClick={() => handleDelete(espacio._id)} className="grow-0 shrink text-[#00FF9D] bg-[#131818] border-[#00FF9D] border dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 ml-2 mt-1">
                             ELIMINAR
                         </button>
                     </div>
