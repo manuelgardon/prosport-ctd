b2010aa4126a96145e5eacd39af6c3b653c54805
victoria <victoria.zazpe@gmail.com>
2023-11-18 18:01:08 -0300

    Estilo Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -51,7 +51,6 @@ export default function Header() {
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to={'/'} className="flex items-center">
                         <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Pro-Sport</span>
                     </Link>
                     {user ? (
                         <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
@@ -81,9 +80,9 @@ export default function Header() {
                     ) :
                         (
                             <div className="flex items-center lg:order-2">
-                                <Link to={'/login'} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in
+                                <Link to={'/login'} className="text-[#A8F2DE] font-semibold text-xl dark:text-white hover:font-bold font-l rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in
                                 </Link>
-                                <Link to={'/register'} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign In
+                                <Link to={'/register'} className="text-[#A8F2DE] font-semibold text-xl dark:text-white hover:font-bold font-l rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign In
                                 </Link>
                                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-[#17B289] dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                                     <span className="sr-only">Open main menu</span>
