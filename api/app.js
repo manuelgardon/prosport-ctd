57769504b2fc9dcc7218ad896a4ea5f80a4bf660
Ubuntu <ubuntu@ip-10-0-6-131.us-west-1.compute.internal>
2023-12-09 03:30:58 +0000

    Comando start y directivas cookies

--- api/app.js
+++ api/app.js
@@ -187,7 +187,7 @@ app.post('/login', async (req, res) => {
       // Creamos un token para el usuario usando su id y email para que se agregue a las cookies con estas propiedades
       jwt.sign({ email: user.email, id: user._id }, process.env.JWT_SECRET, (error, token) => {
         if (error) throw error;
-        res.cookie('token', token, { httpOnly: true, sameSite: 'Lax' }).json(user);
+        res.cookie('token', token, { httpOnly: true, sameSite: 'None', secure: true }).json(user);
       });
     } else {
       res.status(422).json('La contraseña no es valida');
@@ -196,7 +196,7 @@ app.post('/login', async (req, res) => {
 })
 
 app.get('/api/profile', (req, res) => {
-  const { token } = req.cookies;
+ const { token } = req.cookies;
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, {}, async (error, data) => {
       if (error) throw error;
