97af738e1ae660c830a92f772e3510e8cccbe255
Manuel Gardon <manugardon801@gmail.com>
2023-12-11 00:29:54 -0300

    Rama para instancia

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,18 +1,18 @@
 export const data = [
     {
         id:1,
-        imgUrl: "src/assets/images/basket1.png"
+        imgUrl: "./images/basket1.png"
     },
     {
         id:2,
-        imgUrl: "src/assets/images/futbol1.png"
+        imgUrl: "./images/futbol1.png"
     },
     {
         id:3,
-        imgUrl: "src/assets/images/tenis1.png"
+        imgUrl: "./images/tenis1.png"
     },
     {
         id:4,
-        imgUrl: "src/assets/images/voley1.png"
+        imgUrl: "./images/voley1.png"
     }
 ]
